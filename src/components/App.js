3c2304ddaa34b48c5bf5c21cdfd43179039a70bc
Hammad <hammadyasub14@gmail.com>
2022-04-11 13:14:39 +0530

    create PostsList component and render it on app

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions/posts';
+import PostsList from './PostsList';
 
 class App extends React.Component {
   componentDidMount() {
@@ -9,8 +10,12 @@ class App extends React.Component {
   }
 
   render() {
-    console.log('props', this.props);
-    return <div className="App">Hello Codeial</div>;
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
